faa567741b80e21b20053b74a360dfa0cfe007bd
hyorimm <hyorimm@gmail.com>
2023-01-31 20:09:33 +0900

    #4 - feat: 이메일, 비밀번호 유효성 검사

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,20 +1,21 @@
-import { useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
 
 const SignUp = () => {
   const navigate = useNavigate();
-
-  const [disabled, setDisabled] = useState(false);
-
+  const [disabled, setDisabled] = useState(true);
   const [values, setValues] = useState({
     email: "",
     password: "",
   });
-
   const [errors, setErrors] = useState({
     email: "",
     password: "",
+  });
+  const [touched, setTouched] = useState({
+    email: false,
+    password: false,
   });
 
   const handleChange = (e) => {
@@ -22,29 +23,22 @@ const SignUp = () => {
       ...values,
       [e.target.name]: e.target.value,
     });
+    // validate();
   };
 
-  const validate = () => {
-    const emailCheck = () => {
-      var regex = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
-      return values.email !== "" && values.email !== "undefined" && regex.test(values.email);
-    };
-    const errors = {
-      email: "",
-      password: "",
-    };
-    if (!emailCheck(values.email)) {
-      errors.email = "이메일 형식에 맞게 입력 해 주세요";
-    }
-    if (!values.password || values.password.length < 8) {
-      errors.password = "비밀번호를 8자리 이상 입력하세요";
-    }
-
-    return errors;
+  const handleBlur = (e) => {
+    setTouched({
+      ...touched,
+      [e.target.name]: true,
+    });
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    setTouched({
+      email: true,
+      password: true,
+    });
     const errors = validate();
     setErrors(errors);
     if (Object.values(errors).some((v) => v)) {
@@ -52,6 +46,33 @@ const SignUp = () => {
     }
   };
 
+  const validate = useCallback(() => {
+    const errors = {
+      email: "",
+      password: "",
+    };
+
+    const emailCheck = () => {
+      var regex = /([\w-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([\w-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/;
+      return values.email !== "" && values.email !== "undefined" && regex.test(values.email);
+    };
+
+    if (!emailCheck(values.email)) {
+      errors.email = "이메일 형식에 맞게 입력 해 주세요";
+    }
+    if (!values.password || values.password.length < 8) {
+      errors.password = "비밀번호를 8자리 이상 입력하세요";
+    }
+    if (!errors.email && !errors.password) {
+      setDisabled(false);
+    }
+    return errors;
+  }, [values]);
+
+  useEffect(() => {
+    setErrors(validate());
+  }, [validate]);
+  console.log(touched);
   return (
     <Form onSubmit={handleSubmit}>
       <h1>회원 가입</h1>
@@ -65,8 +86,10 @@ const SignUp = () => {
             name="email"
             value={values.email}
             onChange={handleChange}
+            onBlur={handleBlur}
+            // autoFocus
           />
-          {errors.email && <span>{errors.email}</span>}
+          {touched.email && errors.email && <span>{errors.email}</span>}
         </div>
         <div>
           <label htmlFor="password">비밀번호</label>
@@ -77,9 +100,10 @@ const SignUp = () => {
             placeholder="비밀번호를 입력하세요"
             value={values.password}
             onChange={handleChange}
+            onBlur={handleBlur}
           />
         </div>
-        {errors.password && <span>{errors.password}</span>}
+        {touched.password && errors.password && <span>{errors.password}</span>}
       </InputContainer>
       <ButtonContainer>
         <button type="submit" disabled={disabled}>
@@ -121,6 +145,10 @@ const InputContainer = styled.div`
     width: 100%;
     line-height: 10px;
     padding: 0.625rem 0.9375rem;
+    background-color: #ffd4d4;
+    &:focus {
+      background-color: white;
+    }
   }
   span {
     color: red;
@@ -144,6 +172,10 @@ const ButtonContainer = styled.div`
       background-color: #aacb73;
       /* color: white; */
       transition: 0.2s;
+    }
+    :disabled {
+      background-color: grey;
+      cursor: default;
     }
   }
 `;
