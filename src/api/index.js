37e6d33431f171ef1e9177d87c4fa029d3ad78b5
hyorimm <hyorimm@gmail.com>
2023-02-02 19:26:03 +0900

    #7 - feat: 불필요한 콘솔 로그 제거

--- src/api/index.js
+++ src/api/index.js
@@ -28,7 +28,6 @@ export const signIn = async (email, password) => {
         password,
       },
     });
-    console.log("res", res);
     return res.data;
   } catch (error) {
     if (error instanceof AxiosError) {
