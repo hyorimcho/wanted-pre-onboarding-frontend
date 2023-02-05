13b963dd0dc06a8b0b11a1a1b1a9b485e3387915
hyorimm <hyorimm@gmail.com>
2023-02-05 15:09:10 +0900

    #9 - feat: 할 일 목록이 없을 경우 처리

--- src/pages/Todo/Lists.jsx
+++ src/pages/Todo/Lists.jsx
@@ -3,7 +3,7 @@ import List from "./List";
 const Lists = ({ todos, setTodos }) => {
   return (
     <ul>
-      {todos.map((todo) => (
+      {todos?.map((todo) => (
         <List setTodos={setTodos} todos={todos} todo={todo} key={todo.id} />
       ))}
     </ul>
